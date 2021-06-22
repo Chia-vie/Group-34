c214dd75d690d3ee7e6bc641d5744b9e7563ae4a
Chia-vie <christine.ackerl@univie.ac.at>
2021-06-22 15:03:51 +0200

    This is an idea of how the structure could look like: We could have a module that builds the gui (buildgui.py), this is currently not a module but just a normal python file, a module that reads and maybe pre-processes the data using vaex (dataframe.py), a module that takes care of the plotting (plotter.py) and a main file that is executed and calls the modules. I also added some random pictures here just to illustrate the current stage of the gui. Currently the different modules aren't really talking to each other, this needs to be implemented in case we want to follow this structure.

---
+++ buildgui.py
@@ -0,0 +1,88 @@
+# Color schemes
+# http://www.science.smith.edu/dftwiki/index.php/Color_Charts_for_TKinter
+
+import tkinter as tk
+from tkinter import ttk
+from plotter import Plotter
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+
+def pressbutton(choice):
+    global out1
+    # call function
+    plot = Plotter(choice)
+    out.set(plot.plottype())
+    window.update_idletasks()
+
+def openfile():
+    global path
+    path = fd.askopenfilename()
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*'))
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=filename)
+
+# create tkinter window
+window = tk.Tk()
+window.config(bg='pale turquoise')
+
+# Name of the "App"
+window.title('Name')
+# Variables for the output, currently just a string
+out = tk.StringVar()
+out.set('')
+
+# Header in the window
+header = tk.Label(window,
+                  text='*******    We need a cool name for this!    ********',
+                  font=('Helvetica',16, 'bold'), bg='light blue', fg='blue4',
+                  width = 40, height=2)
+# Text in the window
+description = tk.Label(window,
+                       text='Load data and choose a plot-type!',
+                       font=('Helvetica', 16), bg='light blue', fg='black')
+
+# Call preview images
+preview_img_1 = tk.PhotoImage(file='one.png')
+preview_img_2 = tk.PhotoImage(file='two.png')
+preview_img_3 = tk.PhotoImage(file='three.png')
+
+# Show output, this is now a string, need to look how we can have an image
+result = tk.Label(window, textvariable=out, font=('Helvetica',16, 'bold'), bg='light blue', fg='blue4', width = 40, height=2)
+
+# Buttons to choose which plot you want
+plotbutton1 = tk.Button(window, image=preview_img_1, text='Enter', bg='red', fg='orange', command=lambda: pressbutton('1'))
+plotbutton2 = tk.Button(window, image=preview_img_2, text='Enter', bg='red', fg='orange', command=lambda: pressbutton('2'))
+plotbutton3 = tk.Button(window, image=preview_img_3, text='Enter', bg='red', fg='orange', command=lambda: pressbutton('3'))
+
+# Open file button
+open_button = ttk.Button(window, text='Open a File', command=select_file)
+
+# positions
+# row,column
+#  __ __ __
+# |00 01 02|
+# |10 11 12|
+# |20 21 22|
+
+header.grid(row=0, column=0, columnspan=3, rowspan=2)
+description.grid(row=2,column=0, columnspan=3, rowspan=1)
+open_button.grid(row=0, column=0)
+plotbutton1.grid(row=3, column=0)
+plotbutton2.grid(row=3,column=1)
+plotbutton3.grid(row=3, column=2)
+result.grid(row=5, column=0, columnspan=3)
+
+# create window
+window.mainloop()
---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+print('Welcome, I\'m importing a couple of modules.')
+
+from dataframe import DataFrame
+from plotter import Plotter
+from buildgui import BuildGUI
+
+# eventually this has to be some sort of open file button
+path = input('Please enter path to your file')
+
+# check if path is valid and if so read data
+while True:
+    df = DataFrame(path)
+    if df.path == path:
+        df.read_data()
+        break
+    else:
+        path = input(df.msg)
+
