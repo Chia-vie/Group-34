886fcb21ab99bb288639c8a95e75483547d600ec
Chia-vie <christine.ackerl@univie.ac.at>
2021-06-23 20:05:19 +0200

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="Slot",
+    version="0.0.0",
+    author='Laura Ketzer, Christine Ackerl, George Paraschos',
+    author_email='lketzer@aip.de, christine.ackerl@univie.ac.at, gfparaschos@mpifr.de',
+    description='Sliders and Plots',
+    long_description='''This package was developed during the Code/Astro workshop and is a 
+    preliminary version of something cool! =)''',
+    long_description_content_type="text/markdown",
+    url="gitlab.aip.de/...",
+    install_requires=['numpy',
+                      'pandas',
+                      ...
+                      ],
+    keywords='astronomy,plotting,...',
+    packages=setuptools.find_packages(),
+    classifiers=[]
+    '''
+        'Development Status :: 2 - Pre-Alpha',
+        # 3 - Alpha, 4 - Beta, 5 - Production/Stable, 6 - Mature, 7 - Inactive (1 - Planning)
+        'Environment :: Console',
+        'Environment :: Jupyter Notebook',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Scientists',
+        "Programming Language :: Python :: 3",
+    '''
